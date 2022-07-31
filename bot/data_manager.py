73498b7ed57f84c2ae88070ae4dd0eba2b416d12
Dmytro Karpovych <ZAYEC77@gmail.com>
2022-07-31 16:13:48 +0300

    add data sync class

--- bot/data_manager.py
+++ bot/data_manager.py
@@ -1,7 +1,56 @@
+from pprint import pprint
+
+from bot.api.google import GoogleApi
 from bot.log import logging
+
+CAT_APARTMENTS = 'Apartments'
+
+CATEGORIES = {
+    CAT_APARTMENTS: 'Квартири'
+}
+
+PROD_ID = 'id'
+PROD_DISTRICT = 'district'
+
+MAPPING = {
+    PROD_ID: '',
+    PROD_DISTRICT: 'Район',
+}
 
 
 class DataManager:
 
+    def __init__(self):
+        self.api = GoogleApi()
+
     async def sync_data(self):
+        await self.sync_apartments()
+
         logging.info('Done')
+
+    async def sync_apartments(self):
+        data = self.api.get_sheet_data(CATEGORIES[CAT_APARTMENTS])
+
+        header = data[0]
+
+        attr_len = len(MAPPING)
+
+        diff = set(MAPPING.values()) - set(header)
+        if diff:
+            raise Exception(f'Missing columns: {diff}')
+
+        data = data[1:]
+
+        mapped_data = []
+
+        for datum in data:
+            if len(datum) < attr_len:
+                logging.warning('Skipped row: %s', datum)
+                continue
+            print(datum)
+            row = {}
+            for i in range(len(header)):
+                row[header[i]] = datum[i]
+
+            pprint(row)
+            break
